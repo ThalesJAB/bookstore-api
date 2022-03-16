94f32deb1a3096bd28437ac57b5d9fc7175a852e
ThalesJAB <thalesjoseaguiar@gmail.com>
2022-03-16 00:38:52 -0300

    perfil de teste implementado

--- src/main/java/br/com/bookstore/config/TestConfig.java
+++ src/main/java/br/com/bookstore/config/TestConfig.java
@@ -1,39 +1,23 @@
 package br.com.bookstore.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import br.com.bookstore.domain.Categoria;
-import br.com.bookstore.domain.Livro;
-import br.com.bookstore.repositories.CategoriaRepository;
-import br.com.bookstore.repositories.LivroRepository;
+import br.com.bookstore.service.DBService;
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner{
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
+public class TestConfig {
 	
 	@Autowired
-	private LivroRepository livroRepository;
-	@Override
-	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		
-		this.categoriaRepository.save(cat1);
-		this.livroRepository.save(l1);
-		
-		
-		
+	private DBService dbService;
+	
+	
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.dbService.instanciaBaseDeDados();
 	}
 
 }

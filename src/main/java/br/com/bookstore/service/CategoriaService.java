0ec3e3be0e82f087a6b4969f9e99e52a6e8e4c9b
ThalesJAB <thalesjoseaguiar@gmail.com>
2022-03-18 01:07:14 -0300

    findById para Categoria

---
+++ src/main/java/br/com/bookstore/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.bookstore.domain.Categoria;
+import br.com.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		
+		return obj.orElse(null);
+	}
+
+}

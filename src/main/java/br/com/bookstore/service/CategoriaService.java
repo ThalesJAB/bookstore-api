50e695111ffa4fa1bc96ed7047d79c110b647f3a
ThalesJAB <thalesjoseaguiar@gmail.com>
2022-03-20 14:38:18 -0300

    UPDATE para Categoria

--- src/main/java/br/com/bookstore/service/CategoriaService.java
+++ src/main/java/br/com/bookstore/service/CategoriaService.java
@@ -7,28 +7,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.bookstore.domain.Categoria;
+import br.com.bookstore.dtos.CategoriaDTO;
 import br.com.bookstore.repositories.CategoriaRepository;
 import br.com.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encdaleontrado! Id: "+id+", Tipo: " + Categoria.class.getName()));
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto Não Encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDTO) {
+
+		Categoria obj = findById(id);
+
+		obj.setNome(objDTO.getNome());
+		obj.setDescricao(objDTO.getDescricao());
+
+		return repository.save(obj);
+
 	}
 
 }
